0272f372e974fc3239297595a960f9f9b87faf11
aisyah <marioaistwice@gmail.com>
2022-08-24 13:51:19 +0700

    Update Ulang

---
+++ src/components/MenuFooternya.vue
@@ -0,0 +1,8 @@
+<template>
+     <div class="sosialmedia-footer">
+        <a href="https://www.instagram.com/aisrdm_14/" target=”_blank”><img src="@/assets/instagram.svg" alt="" width="35" height="35" /></a>
+        <a href="https://github.com/aisdams/" target=”_blank”><img src="@/assets/github.svg" alt="" width="35" height="35" /></a>
+        <a href="https://www.linkedin.com/in/aisyah-ramadhani-6b010420b/" target=”_blank”><img src="@/assets/linkedin.svg" alt="" width="35" height="35" /></a>
+        <a href="#"><img src="@/assets/facebook.svg" alt="" width="35" height="35" /></a>
+    </div>
+</template>
--- src/views/ContactusView.vue
+++ src/views/ContactusView.vue
@@ -1,9 +1,11 @@
 
 <template>
     <Footernya/>
+    <MenuFooternya/>
 </template>
 
 <script setup lang="ts">
 import "@/contactus.css"
 import Footernya from '../components/Footernya.vue'
+import MenuFooternya from '../components/MenuFooternya.vue'
 </script>
